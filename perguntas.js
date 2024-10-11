9abbb90be0f1dcb22527ef19cefb61ddff748808
Fernandomendesdasilva <160602665+Fernandomendesdasilva@users.noreply.github.com>
2024-10-10 23:29:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criarCARTAO(
+    'O que é Java Script?',
+    'Programação',
+    'É uma linguagem de programação'
+)
+criarCARTAO(
+    'Qual a capital do Brasil?',
+    'Geografia',
+    'A capital do Brasil é Brasília '
+)
+criarCARTAO(
+    'Quanto tempo durou a Segunda Guerra Mundial?',
+    'História',
+    'durou cerca de 6 anos, de 1939 a 1945'
+)
